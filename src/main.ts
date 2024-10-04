d4cf6622f964da1548d91221890007baa139b12a
AlexBollen <alexbollen21@gmail.com>
2024-10-03 19:25:01 -0600

    Implemented swagger orm and endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:"*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  });
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API documentation')
+    .setDescription('Ecommerce RESTful API Official documentation for final project')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
