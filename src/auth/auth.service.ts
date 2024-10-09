5f055ccb219e4b0f5f5b63b6fb3d41862fea409d
AlexBollen <alexbollen21@gmail.com>
2024-10-09 01:47:09 -0600

    Login returns token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,20 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcryptjs from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register({ nombre_persona, username, password }: RegisterDto) {
     const user = await this.userService.findByUsername(username);
@@ -23,13 +31,19 @@ export class AuthService {
   async login({ username, password }: LoginDto) {
     const user = await this.userService.findByUsername(username);
     if (!user) {
-        throw new UnauthorizedException('El username no existe')
+      throw new UnauthorizedException('El username no existe');
     }
 
-    const isPasswordValid = await bcryptjs.compare(password, user.password)
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
     if (!isPasswordValid) {
-        throw new UnauthorizedException('La contraseña no es correcta')
+      throw new UnauthorizedException('La contraseña no es correcta');
     }
-    return user;
+
+    const payload = { username: user.username };
+    const token = await this.jwtService.signAsync(payload);
+    return {
+        token,
+        username
+    }
   }
 }
